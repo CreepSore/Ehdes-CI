76b7691998d59313f6677e1f88fb18a211c92dc9
CreepSore <creepsore@gmail.com>
2020-02-24 13:51:20 +0100

    Added Buildfile to summary

--- Build-Agent/handler/build-handler.js
+++ Build-Agent/handler/build-handler.js
@@ -35,6 +35,7 @@ class Builder {
 
         let summary = {
             workspace: this.workspace,
+            buildfile: this.buildfile,
             success: result,
             buildtime: time,
             date: starttime,
