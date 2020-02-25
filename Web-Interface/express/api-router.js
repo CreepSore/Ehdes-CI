9a3911f74909fc4c0af6c40fd2b03af4e03f99ea
root <root@ehdes.com>
2020-02-25 11:05:52 +0100

    Fixed next call

--- Web-Interface/express/api-router.js
+++ Web-Interface/express/api-router.js
@@ -132,6 +132,9 @@ const registerApi = function(storage, app) {
                 next();
             }
         }
+        else {
+            next();
+        }
     });
 
     register(storage, app);
