b41fa5fbbe9a75e4574a1f69bd68a84186418fe1
CreepSore <ehdes@protonmail.com>
2020-03-16 17:21:50 +0100

    Added Plugin System

---
+++ Agent-Communicator/handler/plugin-management/acom-plugin-interface.js
@@ -0,0 +1,10 @@
+// @ts-nocheck
+"use strict";
+
+class IAcomPlugin extends require("./plugin-interface"){
+    constructor(storage) {
+        super(storage);
+    }
+}
+
+module.exports = IAcomPlugin;
